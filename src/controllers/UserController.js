c438087bbdbacc70800b642da1065e2dd45abc58
CaioZin <sellacaio4@gmail.com>
2024-03-03 20:24:52 -0300

    adicionando validação com jwt criptografia e middleware

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,10 +1,26 @@
 const User = require('../models/User');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 module.exports = {
   async index(req, res){
     const user = await User.findAll();
 
     return res.json(user);
+  },
+
+  async privateRoute(req, res){
+    const id = req.params.id;
+
+    const user = await User.findByPk(id,{
+      attributes: ['id', 'name', 'email']
+    });
+
+    if(!user){
+      return res.status(404).json({msg: 'Usuário não encontrado'});
+    }
+
+    return res.status(200).json({user});
   },
 
   async store(req, res){
@@ -22,8 +38,71 @@ module.exports = {
       return res.status(422).json({msg: 'A senha do usuário é obrigatória'});
     }
 
-    const user = await User.create({name, email, password});
+    if(password !== confirmpassword){
+      return res.status(422).json({msg: 'As senhas não conferem'});
+    }
 
-    return res.json(user);
+    const userExists = await User.findOne({where: {email}});
+
+    if(userExists){
+      return res.status(422).json({msg: 'Já existe um usuário com esse email'});
+    }
+
+    const salt = await bcrypt.genSalt(12);
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    try{
+      await User.create({name, email, password: passwordHash});
+
+      return res.json('Usuário criado com sucesso!');
+    } catch(err){
+      console.log(err);
+
+      return res.status(500).json({msg: 'Erro interno no servidor, não foi possível cadastrar o usuário'});
+    }
+  },
+
+  async authenticate(req, res){
+    const {email, password} = req.body;
+
+    if(!email){
+      return res.status(422).json({msg: 'O email do usuário é obrigatório'});
+    }
+
+    const user = await User.findOne({where: {email}});
+
+    if(!password){
+      return res.status(422).json({msg: 'A senha do usuário é obrigatória'});
+    }
+
+    if(!user){
+      return res.status(422).json({msg: 'Usuário não encontrado'});
+    }
+
+    const passwordIsValid = await bcrypt.compare(password, user.password);
+
+    if(!passwordIsValid){
+      return res.status(422).json({msg: 'Email ou senha incorretos'});
+    }
+
+    try{
+      const secret = process.env.SECRET_KEY;
+
+      const token = jwt.sign(
+        {
+          id: user.id
+        },
+        secret,
+        {
+          expiresIn: 86400
+        }
+      );
+
+      return res.status(200).json({msg: 'Login realizado com sucesso!', token});
+    } catch(err){
+      console.log(err);
+
+      return res.status(500).json({msg: 'Erro interno no servidor, não foi possível realizar o login'});
+    }
   }
 }
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const UserController = require('./controllers/UserController');
+const checkToken = require('./middleware/checkToken');
 
 const routes = express.Router();
 
 routes.get('/users', UserController.index);
+routes.get('/user/:id', checkToken, UserController.privateRoute);
 routes.post('/auth/register', UserController.store);
+routes.post('/auth/login', UserController.authenticate);
 
 module.exports = routes;
